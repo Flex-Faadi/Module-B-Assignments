8518d4fe51dd83a4f68d50258beccebdd601acdf
Fahad khan <fahadk123572@gmail.com>
2021-03-23 18:54:45 +0500

    file access complete

---
+++ app.js
@@ -0,0 +1,21 @@
+//------------DESTRUCTRING OF ARRAY/NESTED ARRAY--------------
+
+// let arr_1=["Fahad","Khan",21,"Front-end Developer",["Work for the Best"]]
+// let [firstName,lastName,age,experties,[moto]] = arr_1;
+// console.log(firstName,lastName,age,experties,moto);
+
+
+//-------------EXTRACTING OF KEYS AND VALUES FROM AN OBJECT--------------
+
+// let obj={
+//     fullName:"Fahad khan",
+//     age:21,
+//     experties:"Front-end Developer",
+//     nestedObj:{
+//         moto:"Work for the Best"
+//     }
+// }
+// let {fullName,age,experties,nestedObj:{moto}} = obj; //object destructuring
+// console.log(fullName,age,experties,moto) 
+// console.log(Object.values(obj)); //For values
+// console.log(Object.keys(obj)); //For keys
