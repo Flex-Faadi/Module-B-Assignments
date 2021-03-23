fd587656b58cab6fb333da617d4060f1d0be6b5d
Fahad khan <75995004+Flex-Faadi@users.noreply.github.com>
2021-03-23 22:05:18 +0500

    updated
    class 3 code

--- app.js
+++ app.js
@@ -21,3 +21,74 @@
 // console.log(fullName,age,experties,moto) 
 // console.log(Object.values(obj)); //For values
 // console.log(Object.keys(obj)); //For keys
+
+//---------------CLASS 3 CODE------------------
+//Combine 2 object in on Object
+
+// let obj_1={
+//     firstName:"Fahad",
+// }
+
+// let obj_2={
+//     lastName:"khan",
+//     ageObject:{
+//         age:22
+//     }
+// }
+//  let newObj=Object.assign(obj_1,obj_2);
+
+//  obj_2.ageObject.age=false;
+//  obj_1.firstName="saad";
+// console.log(newObj);
+// console.log(obj_2);
+
+
+
+//To made true copy of object
+// let obj_2={
+//     lastName:"khan",
+//     ageObject:{
+//         age:22
+//     }
+// }
+
+// let stringify=JSON.parse(JSON.stringify(obj_2))
+// stringify.ageObject.age=25;
+
+// console.log(obj_2)
+// console.log(stringify)
+
+
+
+//take default value in parameter
+// function nameCheck(name="osama") {
+//     let message= `wellcome to ${name}`
+//     console.log(message);
+// }
+
+// nameCheck("saad");
+
+
+//DESTRUCTURE THROUGH FUNNCTION PARAMETER
+
+// function obj({name="fahad Khan",profession="web-developer"}={}) {
+//     let msg=`my Name is ${name} and my profession is ${profession}`
+//     console.log(msg)
+// }
+// obj({name:"Maaz",profession:"devops developer"});
+// obj();
+// console.log("=========================================")
+
+// function obj2({name="fahad Khan",profession="web-developer",...rest}={}) {
+//     let msg=`my Name is ${name} and my profession is ${profession}`
+//     console.log(msg)
+//     console.log(rest)
+// }
+// obj2({name:"Maaz",profession:"devops developer",val1:12,val2:24});
+// obj2();
+
+//SHORT CIRCUIT
+
+// let gender="male";
+// let result=gender=="male" && "blue";
+// console.log(result);
