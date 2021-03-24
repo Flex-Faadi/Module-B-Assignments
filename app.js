a2c358c2cd9e8978fc9fcda26ed8a6866ccce12c
Fahad khan <75995004+Flex-Faadi@users.noreply.github.com>
2021-03-24 17:06:55 +0500

    assignment 2
    You can see assignment  2 edited from line 26 to 87

--- app.js
+++ app.js
@@ -23,3 +23,65 @@
 // console.log(Object.keys(obj)); //For keys
 
 
+//--------ASSIGNMENT "2"-------------
+
+//1- overwrite value while making new object using Object.assign
+// let obj={
+//     fullName:"Fahad Khan",
+//     fatherName:"Sajid Hameed Khan",
+// }
+// let obj2={
+//     age:22,
+//     profession:"Web Development",
+//     hobbyObject:{
+//         hobbies:"Cricket"
+//     }
+// }
+
+// Object.assign(obj,obj2);
+
+// obj2.hobbyObject.hobbies="football"
+// console.log(obj2)
+// console.log(obj)
+
+//2- nested ternary operator
+// function nestedTernary() {
+//     let number=22;
+//     let result=  number < 10 ? "This value is under 10"
+//          : number == 10 ? "This value Equal to 10"
+//          : number > 10 && number < 20 ? "This value is greather than 10 and under 20"
+//          : "This is unknown value";
+//          console.log(result)
+// }
+// nestedTernary();
+
+
+
+//for in and for of loop
+// const student = {
+//     name: 'FAHAD',
+//     class: 13,
+//     age: 21
+// }
+// //FOR-IN LOOP
+// for ( let key in student ) {
+
+//     console.log(`${key} => ${student[key]}`);
+// }
+
+//For-OF Loop
+// let obj=[21,22,33,44,55]
+// for (let key of obj) {
+//     console.log(key);
+// }
+
+// What is arrow function?
+// nestedTernary = () => {
+//     let number=22;
+//     let result=  number < 10 ? "This value is under 10"
+//          : number == 10 ? "This value Equal to 10"
+//          : number > 10 && number < 20 ? "This value is greather than 10 and under 20"
+//          : "This is unknown value";
+//          console.log(result)
+// }
+// nestedTernary();
